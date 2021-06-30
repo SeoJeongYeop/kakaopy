a0c2709c686ca24991a965f769cd57e7a4ff9085
SeoJeongYeop <sjyswe99@gmail.com>
2021-06-30 17:36:38 +0900

    kakaopy 0.1.1

--- setup.py
+++ setup.py
@@ -1,23 +1,20 @@
-import pathlib
-from setuptools import setup
-
-# reading the README file
-HERE = pathlib.Path(__file__).parent
-README = (HERE / "README.md").read_text()
+from setuptools import setup, find_packages
 
 # the setup script
 setup(
     name="kakaopy",
-    version = "0.1.0",
+    version = "0.1.1",
     description = "kakao search API customizing",
-    long_description = README,
+    long_description = open('README.md').read(),
     long_description_content_type = "text/markdown",
     url = "https://github.com/SeoJeongYeop/kakaopy",
     author = "Jeongyeop Seo",
     author_email="sjyswe99@gmail.com",
     license ="MIT",
-    packages = ["kakaopy"],
+    packages = find_packages(exclude=['tests']),
+    zip_safe=False,
     classifiers = [
+        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
